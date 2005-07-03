7b525ee27e3a0e396f98e9b3bdecbcfaec1536e6
Bharat Mediratta <bharat@menalto.com>
2005-07-03 03:44:07 +0000

    Switch from using a Makefile to a bash script to build the packages. Incorporate building nightlies into the script so that our nightlies can use roughly the same process as our official releases.
    git-svn-id: file:///home/jmullan/src/gallery-git/repo/trunk/packaging@10688 57fcd75e-5312-0410-8df3-f5eb6fbb1595

---
+++ gallery2/build.sh
@@ -0,0 +1,68 @@
+#!/bin/sh
+CVSROOT=":ext:`whoami`@cvs.sf.net:/cvsroot/gallery"
+BRANCH="HEAD"
+TMPDIR="`pwd`/tmp"
+DISTDIR="`pwd`/dist"
+CVS="cvs -z3 -d $CVSROOT"
+
+function getVersion {
+    MODULEINC=tmp/gallery2/modules/core/module.inc
+    if [ -f $MODULEINC ]; then
+	VERSION=`grep 'this->setGalleryVersion' $MODULEINC | perl -pe 's/.*\(.(.*).\).*/$1/'`
+    else
+	VERSION="unknown"
+    fi
+}
+
+function checkout {
+    (cd $TMPDIR && $CVS checkout -r $BRANCH gallery2)
+}
+
+function manifest {
+    (cd $TMPDIR/gallery2 && perl lib/tools/bin/makeManifest.pl)
+}
+
+function package {
+    getPackageNames
+    (cd $TMPDIR && \
+	tar czf $TARBALL --exclude CVS gallery2 && \
+	zip -r $ZIPBALL gallery2 -x \*CVS\*)
+}
+
+function getPackageNames {
+    getVersion
+    TARBALL="$DISTDIR/gallery-$VERSION.tar.gz"
+    ZIPBALL="$DISTDIR/gallery-$VERSION.zip"
+}
+
+if [ ! -d $TMPDIR ]; then mkdir $TMPDIR;  fi
+if [ ! -d $DISTDIR ]; then mkdir $DISTDIR; fi
+
+case $1 in
+    nightly)
+	#checkout
+	manifest
+	package
+	;;
+
+    release)
+	# Note: Don't build the manifests for final releases.  When we do a
+	# release, the manifests should be up to date in CVS.  If something
+	# has gone wrong and we're divergent from CVS then building the
+	# MANIFESTs here will obscure that.
+	checkout
+	package
+	;;
+
+    export)
+	ncftpput -u anonymous -p gallery@ upload.sourceforge.net /incoming $TARBALL $ZIPBALL
+	;;
+
+    clean)
+	/bin/rm -rf $TMPDIR
+	;;
+
+    *)
+	echo "Invalid action.  Choices: nightly release export clean"
+	;;
+esac
--- gallery2/getVersion.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-#
-grep 'this->setGalleryVersion' tmp/gallery2/modules/core/module.inc | \
-  perl -pe 's/.*\(.(.*).\).*/$1/'
-
