e4e979f7ca66a128c4e53639608469c524d17fc9
Bharat Mediratta <bharat@menalto.com>
2005-10-14 07:05:19 +0000

    script to generate patch files
    git-svn-id: file:///home/jmullan/src/gallery-git/repo/trunk/packaging@11797 57fcd75e-5312-0410-8df3-f5eb6fbb1595

---
+++ gallery2/buildPatch.php
@@ -0,0 +1,27 @@
+<?php
+/* This cvs should be current for: BRANCH_2_0 */
+$LAST_RELEASE = 'RELEASE_2_0';
+$PATCH = '201';
+
+$patchFile = 'patch' . $PATCH . '.txt';
+system("cvs -q diff -Nur $LAST_RELEASE > $patchFile");
+
+foreach (file($patchFile) as $line) {
+    $str = substr($line, 0, 7);
+    if ($str == 'Index: ') {
+        $changedFile = rtrim(substr($line, 7));
+    } else if ($str == '=======' && !empty($changedFile)) {
+        $changedFiles[] = $changedFile;
+    } else {
+        $changedFile = '';
+    }
+}
+
+$dir = 'changedFiles' . $PATCH;
+mkdir($dir);
+chdir($dir);
+foreach ($changedFiles as $changedFile) {
+    system('mkdir -p ' . dirname($changedFile));
+    system("cp ../$changedFile $changedFile");
+}
+?>
