5b7292c88a03b137910e2fc0f465515caef76f7f
Alan Harder <alan.harder@gmail.com>
2006-04-28 00:02:36 +0000

    Fix 'svn diff' call; use wget instead of GET
    git-svn-id: file:///home/jmullan/src/gallery-git/repo/trunk/packaging@13552 57fcd75e-5312-0410-8df3-f5eb6fbb1595

--- gallery2/changed-plugins.sh
+++ gallery2/changed-plugins.sh
@@ -3,18 +3,19 @@
 # Set CURRENT to where the current files for the next release are located
 # (branches/BRANCH_2_X for patch release, trunk for major release unless
 #  a branch for it has already been created)
-LAST_TAG=RELEASE_2_1
-CURRENT=branches/BRANCH_2_1
+LAST_TAG=RELEASE_2_1_1
+CURRENT=trunk
 
 for pluginFile in `ls modules/*/module.inc themes/*/theme.inc`; do
   plugin=`echo $pluginFile | awk -F/ '{ print $2 }'`
   if [ $plugin = core ]; then continue; fi
-  lastVersion=`GET https://svn.sourceforge.net/svnroot/gallery/tags/${LAST_TAG}/gallery2/${pluginFile} | awk -F"'" '/setVersion/ { print $2; exit }'`
-  #currentVersion=`awk -F"'" '/setVersion/ { print $2; exit }' $pluginFile`
-  currentVersion=`GET https://svn.sourceforge.net/svnroot/gallery/${CURRENT}/gallery2/${pluginFile} | awk -F"'" '/setVersion/ { print $2; exit }'`
+  lastVersion=`wget -q -O - https://svn.sourceforge.net/svnroot/gallery/tags/${LAST_TAG}/gallery2/${pluginFile} | awk -F"'" '/setVersion/ { print $2; exit }'`
+  currentVersion=`wget -q -O - https://svn.sourceforge.net/svnroot/gallery/${CURRENT}/gallery2/${pluginFile} | awk -F"'" '/setVersion/ { print $2; exit }'`
   if [ "$currentVersion" = "$lastVersion" ]; then
     # Check if any files changed for this plugin
-    svn diff --old=https://svn.sourceforge.net/svnroot/gallery/tags/${LAST_TAG}/gallery2 --new=https://svn.sourceforge.net/svnroot/gallery/trunk `dirname $pluginFile` | grep -q '^Index: '
+    svn diff --old=https://svn.sourceforge.net/svnroot/gallery/tags/${LAST_TAG}/gallery2 \
+	     --new=https://svn.sourceforge.net/svnroot/gallery/${CURRENT}/gallery2 \
+	     `dirname $pluginFile` 2>/dev/null | grep -q '^Index: '
     if [ $? -eq 0 ]; then
       echo $plugin
     fi
