aae6b4d69b94f856795c0313770cd1c0730fb06b
Alan Harder <alan.harder@gmail.com>
2006-08-09 00:50:53 +0000

    show status for every plugin
    git-svn-id: file:///home/jmullan/src/gallery-git/repo/trunk/packaging@14233 57fcd75e-5312-0410-8df3-f5eb6fbb1595

--- gallery2/changed-plugins.sh
+++ gallery2/changed-plugins.sh
@@ -11,13 +11,17 @@ for pluginFile in `ls modules/*/module.inc themes/*/theme.inc`; do
   if [ $plugin = core ]; then continue; fi
   lastVersion=`wget -q -O - https://svn.sourceforge.net/svnroot/gallery/tags/${LAST_TAG}/gallery2/${pluginFile} | awk -F"'" '/setVersion/ { print $2; exit }'`
   currentVersion=`wget -q -O - https://svn.sourceforge.net/svnroot/gallery/${CURRENT}/gallery2/${pluginFile} | awk -F"'" '/setVersion/ { print $2; exit }'`
-  if [ "$currentVersion" = "$lastVersion" ]; then
+  if [ "$currentVersion" != "$lastVersion" ]; then
+    printf '%-15s %7s -> %7s'"\n" $plugin "$lastVersion" "$currentVersion"
+  else
     # Check if any files changed for this plugin
     svn diff --old=https://svn.sourceforge.net/svnroot/gallery/tags/${LAST_TAG}/gallery2 \
 	     --new=https://svn.sourceforge.net/svnroot/gallery/${CURRENT}/gallery2 \
 	     `dirname $pluginFile` 2>/dev/null | grep -q '^Index: '
     if [ $? -eq 0 ]; then
-      echo $plugin
+      echo "$plugin $currentVersion needs version bump"
+    else
+      echo "$plugin unchanged"
     fi
   fi
 done
