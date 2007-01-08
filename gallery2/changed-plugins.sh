d07faf53e27c0f829a251da7f79166711588b36d
Alan Harder <alan.harder@gmail.com>
2007-01-08 17:39:42 +0000

    note when only language files have changed for a plugin, bump tag to 2_2_RC_1
    git-svn-id: file:///home/jmullan/src/gallery-git/repo/trunk/packaging@15667 57fcd75e-5312-0410-8df3-f5eb6fbb1595

--- gallery2/changed-plugins.sh
+++ gallery2/changed-plugins.sh
@@ -3,7 +3,7 @@
 # Set CURRENT to where the current files for the next release are located
 # (branches/BRANCH_2_X for patch release, trunk for major release unless
 #  a branch for it has already been created)
-LAST_TAG=RELEASE_2_1_2
+LAST_TAG=RELEASE_2_2_RC_1
 CURRENT=trunk
 
 for pluginFile in `ls modules/*/module.inc themes/*/theme.inc`; do
@@ -17,11 +17,14 @@ for pluginFile in `ls modules/*/module.inc themes/*/theme.inc`; do
     # Check if any files changed for this plugin
     svn diff --old=https://gallery.svn.sourceforge.net/svnroot/gallery/tags/${LAST_TAG}/gallery2 \
 	     --new=https://gallery.svn.sourceforge.net/svnroot/gallery/${CURRENT}/gallery2 \
-	     `dirname $pluginFile` 2>/dev/null | grep -q '^Index: '
-    if [ $? -eq 0 ]; then
-      echo "$plugin $currentVersion needs version bump"
+	     `dirname $pluginFile` 2>/dev/null \
+	| awk '/^Index: (po|locale)\// { S=2 } /^Index: / { exit 1 } END { exit S }'
+    S=$?
+    if [ $S -eq 0 ]; then
+	echo "$plugin unchanged"
     else
-      echo "$plugin unchanged"
+	if [ $S -eq 2 ]; then LANG=' (Language changes only)'; else LANG=; fi
+	echo "$plugin $currentVersion needs version bump$LANG"
     fi
   fi
 done
