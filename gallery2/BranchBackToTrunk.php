16e0fb13473181f4610e6d3235286c3223873e27
Alan Harder <alan.harder@gmail.com>
2007-03-14 16:07:09 +0000

    Script to prepare a series of svn merge and commit commands to merge a development branch back into trunk, adding log messages from the branch into the trunk history.
    git-svn-id: file:///home/jmullan/src/gallery-git/repo/trunk/packaging@16000 57fcd75e-5312-0410-8df3-f5eb6fbb1595

---
+++ gallery2/BranchBackToTrunk.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Prepare a series of svn merge and commit commands to merge a development branch
+ * back into trunk, adding log messages from the branch into the trunk history.
+ */
+$BRANCH='DEV_2_3';
+$STARTREV=15949;
+$BASEURL='https://gallery.svn.sourceforge.net/svnroot/gallery';
+
+$data = $files = array();
+$lastRev = $STARTREV;
+$proc = proc_open("svn log -vr $STARTREV:HEAD $BASEURL/branches/$BRANCH/gallery2",
+		  array(1 => array('pipe', 'w')), $pipe);
+if ($proc) {
+    while ($line = fgets($pipe[1])) {
+	if (!strncmp($line, '----------------------------------------', 40)) {
+	    foreach ($files as $file) {
+		$data[$file] .= $buf . "\n";
+	    }
+	    $files = array();
+	    $buf = '';
+	}
+	else if (preg_match('#^\s*[MAD]\s/branches/' . $BRANCH . '/gallery2/(.*)$#',
+			    $line, $match)) {
+	    $files[] = $match[1];
+	}
+	else if (preg_match('/^Merge.*from trunk/i', $line)) {
+	    // Skip revisions merging changes from trunk
+	    $files = array();
+	    if (!preg_match('/^r(\d+)/', $buf, $match)) die();
+	    if (($mergeRev = $match[1] - 1) > $lastRev) {
+		echo "\nsvn merge -r $lastRev:$mergeRev $BASEURL/branches/$BRANCH/gallery2 .\n";
+	    }
+	    $lastRev = $match[1];
+	}
+	else if (!preg_match('/^(Changed paths:)?$/', $line)) {
+	    $buf .= $line;
+	}
+    }
+    fclose($pipe[1]);
+    proc_close($proc);
+}
+echo "\nsvn merge -r $lastRev:HEAD $BASEURL/branches/$BRANCH/gallery2 .\n";
+
+foreach ($data as $file => $log) {
+    $out[$log][] = $file;
+}
+foreach ($out as $log => $files) {
+    $f = fopen('/tmp/svnlog.' . ++$c, 'w');
+    fwrite($f, "Merge $BRANCH branch back to trunk.  Changes from branch:\n\n" . $log);
+    fclose($f);
+    echo "\nsvn ci -F /tmp/svnlog.$c ", implode(' ', $files), "\n";
+}
+echo "\nrm /tmp/svnlog.*\n\n";
+?>
