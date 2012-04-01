c34f9a92f0cdd68d4e86972b43d8cf06e4f2d33c
Bharat Mediratta <bharat@menalto.com>
2012-04-01 21:58:53 +0000

    Updated for 2.3.2
    git-svn-id: file:///home/jmullan/src/gallery-git/repo/trunk/packaging@20999 57fcd75e-5312-0410-8df3-f5eb6fbb1595

--- gallery2/preinstaller/preinstall.php
+++ gallery2/preinstaller/preinstall.php
@@ -28,7 +28,7 @@ $passPhrase = "";
  * @package Preinstaller
  * @author: Andy Staudacher <ast@gmx.ch>
  * @version $Revision$
- * @versionId 2.3.1
+ * @versionId 2.3.2
  */
 error_reporting(E_ALL);
 set_time_limit(900);
@@ -42,7 +42,7 @@ $downloadUrls = array();
 /*   Latest Release Candidate */
 // $downloadUrls['rc'] = 'http://prdownloads.sourceforge.net/gallery/gallery-2.3-rc-2-full';
 /*   Latest stable release */
-$downloadUrls['stable'] = 'http://prdownloads.sourceforge.net/gallery/gallery-2.3.1-full';
+$downloadUrls['stable'] = 'http://prdownloads.sourceforge.net/gallery/gallery-2.3.2-full';
 /*   Latest Nightly Snapshot */
 $downloadUrls['nightly']= 'http://www.rabinovich.org/G2/gallery-nightly';
 
